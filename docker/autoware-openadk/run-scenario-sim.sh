164f9f69ca7258041bd3ffb8d4a529adb4406f49
oguzkaganozt <oguzkaganozt@gmail.com>
2023-11-27 12:21:23 +0300

    Update run-scenario-sim.sh

--- docker/autoware-openadk/run-scenario-sim.sh
+++ docker/autoware-openadk/run-scenario-sim.sh
@@ -3,6 +3,6 @@
 set -e
 
 # scenario simulator
-docker run -it --net=host -v /dev/shm:/dev/shm  -e ROS_DOMAIN_ID=88 ghcr.io/autowarefoundation/autoware-openadk:runtime-before-planning-humble-x86_64 ros2 launch scenario_test_runner scenario_test_runner.launch.py architecture_type:=awf/universe record:=false scenario:=/autoware/scenario-sim/yield_maneuver_demo.yaml sensor_model:=sample_sensor_kit vehicle_model:=sample_vehicle map_path:=/autoware/scenario-sim/map/
+docker run -it --net=host -v /dev/shm:/dev/shm  -e ROS_DOMAIN_ID=88 ghcr.io/oguzkaganozt/autoware-openadk:runtime-before-planning-humble-aarch64 ros2 launch scenario_test_runner scenario_test_runner.launch.py architecture_type:=awf/universe record:=false scenario:=/autoware/scenario-sim/yield_maneuver_demo.yaml sensor_model:=sample_sensor_kit vehicle_model:=sample_vehicle map_path:=/autoware/scenario-sim/map/
 
 
